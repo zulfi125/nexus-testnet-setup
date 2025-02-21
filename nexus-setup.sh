ed7f0ed0d2e9e3e7809a85ac406589df9b6a7c01
zulfiqar ali <35648649+zulfi125@users.noreply.github.com>
2025-02-21 14:31:38 +0500

    Update nexus-setup.sh

--- nexus-setup.sh
+++ nexus-setup.sh
@@ -3,9 +3,6 @@
 # Update system packages
 sudo apt update
 
-# Install dependencies
-sudo apt install -y pkg-config libssl-dev protobuf-compiler
-
 # Download and install the latest Protocol Buffers (protobuf)
 wget https://github.com/protocolbuffers/protobuf/releases/download/v30.0-rc1/protoc-30.0-rc-1-linux-x86_64.zip
 sudo unzip protoc-30.0-rc-1-linux-x86_64.zip -d /usr/local/
