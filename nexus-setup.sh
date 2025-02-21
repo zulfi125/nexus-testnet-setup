d6f6da61ed4072b751fd86fe46f00c9d619c0ac2
zulfiqar ali <35648649+zulfi125@users.noreply.github.com>
2025-02-21 16:19:54 +0500

    Update nexus-setup.sh

--- nexus-setup.sh
+++ nexus-setup.sh
@@ -3,14 +3,22 @@
 # Update system packages
 sudo apt update
 
-# Install dependencies
-sudo apt install -y pkg-config libssl-dev protobuf-compiler
+# Install required dependencies
+sudo apt install -y build-essential gcc g++ make cmake pkg-config libssl-dev protobuf-compiler
+
+# Remove existing protobuf-compiler (if any)
+sudo apt-get remove -y protobuf-compiler
+
+# Download and install the latest Protocol Buffers (protobuf)
+wget https://github.com/protocolbuffers/protobuf/releases/download/v30.0-rc1/protoc-30.0-rc-1-linux-x86_64.zip
+sudo unzip protoc-30.0-rc-1-linux-x86_64.zip -d /usr/local/
+sudo chmod +x /usr/local/bin/protoc
 
 # Create and navigate to the nexus-cli directory
 mkdir -p nexus-cli
 cd nexus-cli
 
-# Install Rust and set up environment
+# Install Rust and set up the environment
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
@@ -22,3 +30,9 @@ sudo apt upgrade -y git
 
 # Install Nexus CLI
 curl https://cli.nexus.xyz/ | sh
+
+# Verify installation
+rustc --version
+cargo --version
+gcc --version
+which cc
